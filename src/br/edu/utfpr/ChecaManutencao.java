399e0715024f9708f7531f0392bfa155bc69f4a7
ValcirJr <balbinottijr@outlook.com>
2022-05-04 21:58:09 -0300

    Initial commit

---
+++ src/br/edu/utfpr/ChecaManutencao.java
@@ -0,0 +1,21 @@
+package br.edu.utfpr;
+
+import java.time.LocalDate;
+
+public class ChecaManutencao {
+
+    public static Boolean checaManutencao(Maquina maquina, int mesesParaManutencao) {
+        LocalDate dataManutencaoNecessaria =
+                getDataManutencaoNecessaria(maquina, mesesParaManutencao);
+        if(dataManutencaoNecessaria.isBefore(LocalDate.now())){
+            return true;
+        }
+        return false;
+    }
+
+    private static LocalDate getDataManutencaoNecessaria(Maquina maquina, int mesesParaManutencao) {
+        return maquina.getUltimaManutencao().plusMonths(mesesParaManutencao);
+    }
+
+
+}
